ebdcb75fe49b7ac93eef3921c2b692bc450b1c31
ktscates <cates2506@gmail.com>
2019-08-05 19:35:48 +0200

    Change js file, add alert for total and deliver and add paragraph tag to display message

--- js/script.js
+++ js/script.js
@@ -18,82 +18,109 @@ $(document).ready(function(){
         if($("#small").prop("checked")){
             $("#medium").prop("checked",false);
             $("#large").prop("checked",false);
-            var sizeofpizza=$("#small").val()+": 1000";
-            console.log(sizeofpizza)            
+            var sizeofpizza=$("#small").val();
+            console.log("small"+sizeofpizza)            
         }
         else if($("#medium").prop("checked")){
             $("#small").prop("checked",false);
             $("#large").prop("checked",false);
-            var sizeofpizza=$("#medium").val()+": 2000";
+            var sizeofpizza=$("#medium").val();
             console.log(sizeofpizza)            
         }
         else if($("#large").prop("checked")){
             $("#small").prop("checked",false);
             $("#medium").prop("checked",false);
-            var sizeofpizza=$("#large").val()+": 3000";
+            var sizeofpizza=$("#large").val();
             console.log(sizeofpizza)            
         }
         
-        if($("#cripsy").prop("checked")){           
+        if($("#crispy").prop("checked")){           
             $("#stuffed").prop("checked",false);
             $("#gluten").prop("checked",false);
-            var crustofpizza=$("#crispy").val()+": 4000";
+            var crustofpizza=$("#crispy").val();
             console.log(crustofpizza)            
         }
         else if($("#stuffed").prop("checked")){           
             $("#crispy").prop("checked",false);
             $("#gluten").prop("checked",false);
-            var crustofpizza=$("#stuffed").val()+": 5000";
+            var crustofpizza=$("#stuffed").val();
             console.log(crustofpizza)            
         }
         else if($("#gluten").prop("checked")){
             $("#crispy").prop("checked",false);
             $("#stuffed").prop("checked",false);
-            var crustofpizza=$("#gluten").val()+": 6000";
+            var crustofpizza=$("#gluten").val();
             console.log(crustofpizza)            
         }
-
         var toppingofpizza=[];
         
         $("input[type=checkbox]:checked").each(function(){
             toppingofpizza.push($(this).val());
             
+            console.log(toppingofpizza)
         }); 
-        console.log(toppingofpizza)
 
         var priceofpizza=0;
         for(var i=0; i<toppingofpizza.length;i++){
             if(toppingofpizza[i]==="Pepperoni"){
-                toppingofpizza=$("#pepperoni").val()
-                priceofpizza +=1000;
+                priceofpizza+=1000
                 console.log(priceofpizza)
             }
             else if(toppingofpizza[i]==="Sausage"){
-                priceofpizza +=2000;
+                priceofpizza+=1000
                 console.log(priceofpizza)
             }
             else if(toppingofpizza[i]==="Onions"){
-                priceofpizza +=3000;
+                priceofpizza+=1000
                 console.log(priceofpizza)
             }
             else if(toppingofpizza[i]==="Mushrooms"){
-                priceofpizza +=4000;
+                priceofpizza+=1000
                 console.log(priceofpizza)
             }
             else if(toppingofpizza[i]==="Extra Cheese"){
-                priceofpizza +=5000;
+                priceofpizza+=1000
                 console.log(priceofpizza)
             }
         }
 
+        if($("#yes").prop("checked")){
+            $("#no").prop("checked",false);
+            var delivery=$("#yes").val();
+            console.log(delivery)            
+        }
+        else if($("#no").prop("checked")){
+            $("#yes").prop("checked",false);
+            var delivery=$("#no").val();
+            console.log(delivery)
+        }
+        
 
-        var newPizza=new OrderPizza(sizeofpizza,crustofpizza,toppingofpizza,priceofpizza);
+
+        var newPizza=new OrderPizza(sizeofpizza,crustofpizza,toppingofpizza,priceofpizza,delivery);
 
         console.log(newPizza)
         $("#displayOrder").show();
-        $("#displayOrder").append(newPizza.size)
-        $("#displayOrder").append(newPizza.crust)
-        $("#displayOrder#pizzaTopping").append(newPizza.topping)
+        $("#displayOrder").append("<p>"+newPizza.size+"<p>")
+        $("#displayOrder").append("<p>"+newPizza.crust+"<p>")
+        $("#displayOrder").append("<p>"+newPizza.price+"<p>")
+        $("#displayOrder").append("<p>"+newPizza.deliver+"<p>")
+
+        var total=parseInt(newPizza.size)+parseInt(newPizza.crust)+parseInt(newPizza.deliver)
+         
+        var nprice = newPizza.price
+
+        for(var a=0; a < nprice.length; a++){
+
+            total+=parseInt(nprice[a])
+            console.log(parseInt(nprice[a]))
+        }
+        // +parseInt(newPizza.price);
+        
+
+        // $("#displayOrder").append("<p>"+""+total+"<p>")
+        alert("The total price is "+total);
+
     });
 
 });
